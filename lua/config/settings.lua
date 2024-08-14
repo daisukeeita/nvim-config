1e6189160b7a2113d2837a9e34176d75bd3dd4e2
JAVV <78518239+daisukeeita@users.noreply.github.com>
2024-08-14 11:57:50 +0800

    updating the settings

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,17 +1,48 @@
 local opt = vim.opt
 
-opt.clipboard = "unnamedplus"
 
--- UI CONFIGURATION --
-opt.expandtab = true
-opt.shiftwidth = 2
-opt.softtabstop = 2
-opt.tabstop = 2
-opt.termguicolors = true
-opt.updatetime = 250
+opt.clipboard = 'unnamedplus'
+opt.completeopt = {'menu', 'menuone', 'noinsert', 'noselect'}
 
+
+ -- TAB --
+opt.expandtab = true	-- TABS are using spaces
+opt.softtabstop = 2	-- number of spaces in TAB when editing
+opt.shiftwidth = 2	-- insert 2 space when TAB is pressed
+opt.tabstop = 2		-- number of visual spaces per TAB
+
+ -- UI CONFIG -- 
+opt.cursorline = true
 opt.number = true
 opt.relativenumber = true
+opt.splitbelow = true
+opt.splitright = true
+opt.showmode = false
+opt.termguicolors = true
+
+opt.laststatus = 3
+opt.scrolloff = 10
+opt.textwidth = 78
+opt.updatetime = 250
+
+ -- INDENT --
+opt.autoindent = true
+opt.breakindent = true
+opt.smartindent = true
+opt.wrap = true
+
+ -- SEARCHING --
+opt.hlsearch = false
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+
+ -- SPELLING --
+opt.encoding = 'UTF-8'
+
+ -- FILE -- 
+opt.swapfile = false
+opt.title = true
 
 -- DIAGNOSTIC CONFIGURATIONS --
 vim.diagnostic.config({
